962688381d99ba1a91a23d4d13c2682d1729911c
Joe Blau <jblau@uberatc.com>
2018-02-08 14:58:36 -0500

    Added UI

--- Podfile
+++ Podfile
@@ -2,19 +2,16 @@
 # platform :ios, '9.0'
 
 target 'Testarosa' do
-  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for Testarosa
+  pod 'Surge', '~> 2.0.0'
 
   target 'TestarosaTests' do
     inherit! :search_paths
-    # Pods for testing
   end
 
   target 'TestarosaUITests' do
     inherit! :search_paths
-    # Pods for testing
   end
 
   target 'TestarosaSnapshotTests' do
--- Testarosa/ViewController.swift
+++ Testarosa/ViewController.swift
@@ -7,19 +7,76 @@
 //
 
 import UIKit
+//import Surge
 
 class ViewController: UIViewController {
 
+    private var randomButton: UIButton
+    private var sumLabel: UILabel
+    
+    init() {
+        randomButton = UIButton(type: .system)
+        sumLabel = UILabel()
+
+        super.init(nibName: nil, bundle: nil)
+
+        randomButton.setTitleColor(.white, for: .normal)
+        randomButton.translatesAutoresizingMaskIntoConstraints = false
+        randomButton.setTitle("Random Sum", for: .normal)
+        randomButton.backgroundColor = .blue
+        randomButton.addTarget(self, action: #selector(generateRandomSum(_:)), for: .touchUpInside)
+        
+        sumLabel.translatesAutoresizingMaskIntoConstraints = false
+        sumLabel.textAlignment = .center
+        sumLabel.text = "?"
+        sumLabel.backgroundColor = .lightGray
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        
+        addSubviews()
+        layoutViews()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func addSubviews() {
+        view.addSubview(randomButton)
+        view.addSubview(sumLabel)
     }
+    
+    func layoutViews() {
+        randomButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        randomButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+        randomButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        randomButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
+        
+        
+        sumLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        sumLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+        sumLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        sumLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100).isActive = true
+    }
+    
+    // MARK: - Target Actions
+    
+    @objc func generateRandomSum(_ sender: UIButton) {
+        let randomSubBillion = Int(arc4random_uniform(1000))
+        let randomSum = add(numbers: Array(0...randomSubBillion))
+        sumLabel.text = "\(randomSum)"
+    }
+    
+    // MARK: - Private
 
+    func add(numbers: [Int]) -> Int {
+        return numbers.reduce(0, +)
+    }
 
 }
 
